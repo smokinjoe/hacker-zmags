a65e1c54761c6adb54659347ce47cdc60cd3442c
Joe <joe.ekiert@gmail.com>
2018-05-25 22:11:59 -0700

    rough initial layout in place

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,45 +3,53 @@ import React, { Component } from 'react';
 class Main extends Component {
 
   renderRow () {
-    return (
-      <div className="row">
-        <div className="col-1">
-          <span className="story-score">420</span>
-        </div>
-        <div className="col-9">
-          <h3>
-            <a className="story-link" href="#">
-              Lorem ipsum dolor sit amet sit amet title.
-            </a>
-          </h3>
-          <h4>
-            <span className="story-author">
-              <span className="story-author-score">
-                50
+    const rows = [];
+
+    for (let i = 0; i < 10; i++) {
+      rows.push(
+        <div className="row">
+          <div className="col-1">
+            <span className="story-score">420</span>
+          </div>
+          <div className="col-9">
+            <h3>
+              <a className="story-link" href="#">
+                Lorem ipsum dolor sit amet sit amet title.
+              </a>
+            </h3>
+            <h4>
+              <span className="story-author">
+                <small>
+                  <span className="story-author-score">
+                    50
+                  </span>
+                  Ed Bast.
+                </small>
               </span>
-              Ed Bast.
-            </span>
-          </h4>
-        </div>
-        <div className="col-2">
-          <span className="story-timestamp">7/17/2018</span>
+            </h4>
+          </div>
+          <div className="col-2">
+            <span className="story-timestamp">7/17/2018</span>
+          </div>
+
         </div>
 
-      </div>
+      );
+    }
 
-    );
+    return rows;
   }
 
   render () {
     return (
       <div className="container">
+        <div class="page-header mt-3 mb-4">
+          <h1>Welcome to my Hacker News viewer <small>scroll on down!</small></h1>
+        </div>
 
         { this.renderRow() }
 
       </div>
-
-
-
 
     );
   }
